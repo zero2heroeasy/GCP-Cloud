ef45cc9cf16da746e76d1cdb13c554d147953c29
zero2heroeasy <zero2heroeasy@gmail.com>
2023-07-15 02:19:24 +0530

    updated code

--- GCP-Terraform/Terraform-GCP-Compute-Disks/main.tf
+++ GCP-Terraform/Terraform-GCP-Compute-Disks/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.73.1"
-    }
-  }
-}
-provider "google" {
-  project = "terraform-training-392804"
-  region = "us-east1"
-### Create SA keys and add json to the current project folder ###
-  credentials = "keys.json"
-}
 resource "google_compute_disk" "tf-disk" {
   name  = "tf-disk"
   description = "this disk is created from terraform script"
