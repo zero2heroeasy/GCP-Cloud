8e08024e92a375cc63fcb3f18875f1ced0eb9b5b
zero2heroeasy <138552899+zero2heroeasy@users.noreply.github.com>
2023-07-14 13:20:34 +0530

    Update bucket-creation-with-service-account-authentication.tf

--- GCP-Terraform/bucket-creation-with-service-account-authentication.tf
+++ GCP-Terraform/bucket-creation-with-service-account-authentication.tf
@@ -10,8 +10,9 @@ provider "google" {
   project = "terraform-training-392804"
   region = "us-east1"
   zone = "us-east1-a"
+### Create SA keys and add json to the current project folder ###
+  credentials = "keys.json"
 }
-### create cloud storage bucket with service account authentication process ###
 resource google_storage_bucket "GCS2" {
   name = "bucket-creation-with-service-account-authentication"
   location = "us-east1"
