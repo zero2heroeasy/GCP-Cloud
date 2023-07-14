beff5dc7080e049142904f32a90a3aa3eb21f51b
zero2heroeasy <138552899+zero2heroeasy@users.noreply.github.com>
2023-07-14 12:59:32 +0530

    Create Storage-bucket-with-service-account.tf

---
+++ GCP-Terraform/Storage-bucket-with-service-account.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.73.1"
+    }
+  }
+}
+provider "google" {
+  project = "terraform-training-392804"
+  region = "us-east1"
+  zone = "us-east1-a"
+}
+resource google_storage_bucket "GCS2" {
+  name = "bucket-creation-with-service-account-authentication"
+  location = "us-east1"
+}
